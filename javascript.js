91d1f6fed5aa9d1adca30d843a83f07141e99bc7
rloftus215 <rloftus215@gmail.com>
2024-06-05 11:51:58 +0100

    Create function to get human choice

--- javascript.js
+++ javascript.js
@@ -8,4 +8,18 @@ function getComputerChoice() {
         console.log('Computer: scissors');
     };
  };
+
+ function getHumanChoice() {
+    let humanChoice = prompt('Type a choice (Rock, paper or scissors) to play');
+ 
+ 
+    if (humanChoice === 'rock') {
+        console.log('Your choice: rock');
+    } else if (humanChoice === 'paper') {
+        console.log('Your choice: paper');
+    } else {
+        console.log('Your choice: scissors');
+    };
+ };
+ 
  
